d5ecf455891a6ba6ade91fdcba7be3d45fcac745
Hakan ÖZER <hakanozer02@gmail.com>
2024-11-19 15:58:51 +0300

    Create FilterConfig.java

---
+++ Product/src/main/java/com/works/configs/FilterConfig.java
@@ -0,0 +1,24 @@
+package com.works.configs;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class FilterConfig implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String url = request.getRequestURI();
+        System.out.println("url : " + url);
+
+        filterChain.doFilter(request, response);
+    }
+
+}
